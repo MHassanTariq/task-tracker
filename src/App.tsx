2c8092c8d2e37f0347bc25a6f7198a494baeb40e
IrzumAdeel <irzumadeel48@gmail.com>
2024-10-24 14:46:54 +0500

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import Home from "./page/home/home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Backlog from "./page/home/backlog";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "backlog",
+    element: <Backlog />,
+  },
+]);
 
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
