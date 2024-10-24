20ec1cc48a98fbaa4f2cd2fb53f41313ce281632
M Hassan Tariq <mhtariq29@gmail.com>
2024-10-24 17:06:57 +0500

    feat: added backlog UI and refactored already made components

--- src/page/backlog/backlog.tsx
+++ src/page/backlog/backlog.tsx
@@ -1,16 +1,74 @@
 import React from "react";
 import PageScreen from "../../components/pageScreen";
 import colors from "../../utils/colors";
+import styles from "../../utils/styles";
+import TaskCreationForm from "../../components/taskCreationForm";
+import { StandardButton } from "../../components/standardButton";
+import DraggableTaskList, {
+  DraggableTaskProps,
+} from "../../components/taskList";
+import { converTaskToDraggableTask } from "../../helpers/taskHelpers";
 
 function Backlog() {
+  function onAdd() {}
+  function onMove() {}
+  function onDiscard() {}
+  const taskOperations = {
+    onDelete: onMove,
+    onToggle: onMove,
+    editTask: onMove,
+    setEditingTaskId: onMove,
+  };
+  const taskList: DraggableTaskProps = {
+    task: {
+      id: "123",
+      text: "Do smth",
+      isCompleted: false,
+    },
+    isHighlighted: true,
+    disableRightOptions: true,
+  };
+
+  const areOptionsDisabled = false;
+
   function Title() {
     return <p className={`${colors.titleText}`}>Backlog</p>;
   }
 
+  function StandardButtonOptions() {
+    return (
+      <div className={`${styles.flexRow} gap-5 pt-4`}>
+        <StandardButton
+          type="button"
+          onClick={onMove}
+          text="Move"
+          disabled={areOptionsDisabled}
+        />
+        <StandardButton
+          type="button"
+          onClick={onDiscard}
+          text="Discard"
+          disabled={areOptionsDisabled}
+        />
+      </div>
+    );
+  }
+
   return (
-    <PageScreen>
+    <PageScreen onDragEnd={() => {}}>
       <Title />
-      {/* Add your backlog items here */}
+      <div className={styles.bodyArea}>
+        <TaskCreationForm
+          onSubmitTask={onAdd}
+          placeholder="🚀 Add your next big idea here..."
+        />
+        <StandardButtonOptions />
+        <DraggableTaskList
+          taskList={[taskList]}
+          listId={"backlogList"}
+          taskOperations={taskOperations}
+        />
+      </div>
     </PageScreen>
   );
 }
