20f4fe684d218e21e73955cbd8aaaf07b01b6b3c
M Hassan Tariq <mhtariq29@gmail.com>
2024-10-25 21:48:35 +0500

    feat: added icons in the sidebar

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,28 +1,40 @@
+import { BacklogIcon } from "../assets/svgs/backlogIcon";
 import Logo from "../assets/svgs/logo";
+import { TaskListIcon } from "../assets/svgs/taskListIcon";
 import colors from "../utils/colors";
 import { Link } from "react-router-dom";
+import styles from "../utils/styles";
 
 function SideBar() {
+  const unselectedStyles = `flex flex-1 ${colors.text} px-2 py-1`;
+  const selectedStyles = `${colors.btnGradient} rounded-full font-bold ${unselectedStyles}`;
+
+  function renderLinkAndIcon(
+    path: string,
+    text: string,
+    className: string,
+    Icon: React.FC
+  ) {
+    return (
+      <div className={`flex ${styles.verticalCenter} ${className} gap-2`}>
+        <Icon />
+        <Link to={path}>{text}</Link>
+      </div>
+    );
+  }
+
   return (
-    <div className={`xsm:hidden lg:flex flex-col p-7 ${colors.navBarBg} w-60`}>
+    <div className={`xsm:hidden lg:flex flex-col p-5 ${colors.navBarBg} w-60`}>
       <Logo />
-      <ul className="mt-8  space-7-8">
-        <li className="flex items-center">
-          <Link
-            to="/Home"
-            className={`${colors.btnGradient} flex flex-1 rounded-full ${colors.text} px-2 py-1`}
-          >
-            TaskBoard
-          </Link>
-        </li>
-      </ul>
-      <ul className="mt-9 space-y-9">
-        <li className="flex items-center">
-          <Link to="/backlog" className="btnGradient">
-            Backlog
-          </Link>
-        </li>
-      </ul>
+      <div className="mt-8 gap-5 flex flex-col">
+        {renderLinkAndIcon("/", "Task Board", selectedStyles, TaskListIcon)}
+        {renderLinkAndIcon(
+          "/backlog",
+          "Backlog",
+          unselectedStyles,
+          BacklogIcon
+        )}
+      </div>
     </div>
   );
 }
