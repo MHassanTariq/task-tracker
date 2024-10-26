9ffa212a2edeb53046d22e4ded6dbacf179f450b
M Hassan Tariq <mhtariq29@gmail.com>
2024-10-26 06:31:12 +0500

    feat: updated the design for mobile drawer

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,3 +1,5 @@
+import colors from "./colors";
+
 const styles = {
   horizontalCenter: "justify-center",
   centerChildren: "justify-center items-center",
@@ -10,6 +12,8 @@ const styles = {
   bodyArea: "flex flex-1 flex-col pt-5 lg:px-14 gap-5 ",
   standardBtn:
     "font-bold text-base border-2 rounded flex justify-center items-center",
+  sideBarSelectedItem: `${colors.btnGradient} rounded-full font-bold flex flex-1 ${colors.text} px-4 py-1`,
+  sideBarUnselectedItem: `flex flex-1 ${colors.text} px-4 py-1`,
 };
 
 export default styles;
