02056f8b287235e51ea9d6d2af5995ce69495a01
M Hassan Tariq <mhtariq29@gmail.com>
2024-10-26 06:31:12 +0500

    feat: updated the design for mobile drawer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import styles from "../utils/styles";
+import DrawerMenu from "./drawerMenu";
 import { PopupOption } from "./optionsPopup";
 import { PrimaryButton, PrimaryButtonVariant } from "./primaryButton";
 
@@ -15,20 +16,25 @@ interface Props {
 
 function Header({ buttons }: Props) {
   return (
-    <header
-      className={` p-7 ${styles.flexRow} ${styles.verticalCenter} justify-end gap-5`}
+    <div
+      className={`p-7 ${styles.flexRow} justify-between ${styles.verticalCenter}`}
     >
-      {buttons.map((button, index) => (
-        <PrimaryButton
-          type="button"
-          onClick={button.onClick ?? undefined}
-          variant={button.variant}
-          text={button.text}
-          key={index}
-          options={button.options}
-        />
-      ))}
-    </header>
+      <DrawerMenu />
+      <header
+        className={`${styles.flexRow} ${styles.verticalCenter} justify-end gap-5`}
+      >
+        {buttons.map((button, index) => (
+          <PrimaryButton
+            type="button"
+            onClick={button.onClick ?? undefined}
+            variant={button.variant}
+            text={button.text}
+            options={button.options}
+            key={index}
+          />
+        ))}
+      </header>
+    </div>
   );
 }
 
