e966daf574bd5bc1864ed133ba51a20641e26a51
IrzumAdeel <irzumadeel48@gmail.com>
2024-11-04 18:42:58 +0500

    Added React Modal

--- src/components/reactModal.tsx
+++ src/components/reactModal.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import WhatsNewModal from "./whatsNewModal";
 
 const ReactModal: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -10,7 +10,7 @@ const ReactModal: React.FC = () => {
   return (
     <div className="App">
       <button onClick={openModal}></button>
-      <WhatsNewModal isOpen={isModalOpen} onRequestClose={closeModal} />
+      <WhatsNewModal />
     </div>
   );
 };
